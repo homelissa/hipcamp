bf3db33dea91f75e694e1c0241efcfc1068a292f
Melissa Ho <homelissa93@gmail.com>
2018-06-06 11:57:27 -0700

    Add Reducers and Store

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_054051) do
+ActiveRecord::Schema.define(version: 2018_06_06_173939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address"
-    t.string "password_digest"
-    t.string "session_token"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, signup, logout } from './util/session_api_util.js'
+import configureStore from './store/store.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
 
+  // TESTING START
   window.login = login;
   window.signup = signup;
   window.logout = logout;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // TESTING END
 
+  const store = configureStore();
   ReactDOM.render(<h1>Welcome to hipCamp</h1>, root);
 });
