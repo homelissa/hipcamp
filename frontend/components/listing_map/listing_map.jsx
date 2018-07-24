010089912e4adb1b466f0db14473bc845c0e29f8
Melissa Ho <homelissa93@gmail.com>
2018-07-23 22:26:12 -0700

    attempt listing map dynamic recenter based on search query

--- frontend/components/listing_map/listing_map.jsx
+++ frontend/components/listing_map/listing_map.jsx
@@ -40,12 +40,17 @@ class ListingMap extends React.Component {
   }
 
   componentWillReceiveProps(newProps) {
-    if (this.props.listings !== newProps.listings) {
-      this.setState({listings: newProps.listings})
-    }
+    // if (Object.keys(this.props.listings)[0] !== Object.keys(newProps.listings)[0]) {
+    //   this.setState({listings: newProps.listings})
+    // }
+    // if (Object.keys(this.props.listings)[0] !== Object.keys(newProps.listings)[0]) {
+    //   this.setState({listings: newProps.listings});
+    //   this.recenterMap();
+    // }
 
 
   }
+
 
 
 
@@ -71,16 +76,34 @@ class ListingMap extends React.Component {
     });
   }
 
-  recenterMap() {
-    console.log(this.props.listings)
+  // recenterMap() {
+  //   console.log(this.props.listings)
+  //
+  //   const listing = Object.values(this.props.listings)[0]
+  //   if (listing) {
+  //
+  //     console.log('hits recenter')
+  //     console.log(listing)
+  //
+  //     this.map.setCenter({lat: listing.lat, lng: listing.lng})
+  //   }
+  // }
 
+  recenterMap() {
+    // console.log(this.props.listings)
+    //
     const listing = Object.values(this.props.listings)[0]
     if (listing) {
-
-      console.log('hits recenter')
-      console.log(listing)
-
-      this.map.setCenter({lat: listing.lat, lng: listing.lng})
+    //   //
+    //   // console.log('hits recenter')
+    //   // console.log(listing)
+     this.map.setCenter({lat: listing.lat, lng: listing.lng})
+     const { north, south, east, west } = this.map.getBounds().toJSON();
+     const bounds = {
+       northEast: { lat: north, lng: east },
+       southWest: { lat: south, lng: west }
+     };
+     this.props.updateFilter('bounds', bounds);
     }
   }
 
@@ -120,3 +143,121 @@ class ListingMap extends React.Component {
 }
 
 export default ListingMap;
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { withRouter } from 'react-router-dom';
+// import MarkerManager from '../../util/marker_manager'
+//
+//
+// const getCoordsObj = latLng => ({
+//   lat: latLng.lat(),
+//   lng: latLng.lng()
+// });
+//
+// const mapOptions = {
+//   center: {
+//     lat: 37.7899,
+//     lng: -122.4454
+//   },
+//   zoom: 12
+// };
+//
+//
+//
+//
+//
+// class ListingMap extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       listings: this.props.listings
+//     }
+//   }
+//
+//
+//   componentDidMount() {
+//     const map = this.refs.map;
+//     this.map = new google.maps.Map(map, mapOptions)
+//     this.MarkerManager = new MarkerManager(this.map)
+//     this.registerListeners();
+//     this.geocoder = new google.maps.Geocoder;
+//
+//   }
+//
+//   componentWillReceiveProps(newProps) {
+//     if (Object.keys(this.props.listings)[0] !== Object.keys(newProps.listings)[0]) {
+//       this.setState({listings: newProps.listings});
+//     }
+//     this.recenterMap();
+//   }
+//
+//   registerListeners() {
+//     google.maps.event.addListener(this.map, 'idle', () => {
+//       const { north, south, east, west } = this.map.getBounds().toJSON();
+//       const bounds = {
+//         northEast: { lat:north, lng: east },
+//         southWest: { lat: south, lng: west } };
+//       this.props.updateFilter('bounds', bounds);
+//     });
+//
+//     google.maps.event.addListener(this.map, 'click', (event) => {
+//       const coords = getCoordsObj(event.latLng);
+//       this.handleClick(coords);
+//       // this.map.setCenter(coords)
+//     });
+//   }
+//
+//   recenterMap() {
+//     // console.log(this.props.listings)
+//     //
+//     const listing = Object.values(this.state.listings)[0]
+//     if (listing) {
+//     //   //
+//     //   // console.log('hits recenter')
+//     //   // console.log(listing)
+//      this.map.setCenter({lat: listing.lat, lng: listing.lng})
+//      // const { north, south, east, west } = this.map.getBounds().toJSON();
+//      // const bounds = {
+//      //   northEast: { lat: north, lng: east },
+//      //   southWest: { lat: south, lng: west }
+//      // };
+//      // this.props.updateFilter('bounds', bounds);
+//     }
+//   }
+//
+//
+//
+//  //  recenterMap() {
+//  //   this.geocoder.geocode({ address: this.state.searchQuery }, (result, status) => {
+//  //     if (status === 'OK') {
+//  //       const newCenter = result[0].geometry.location;
+//  //       this.map.setCenter(newCenter);
+//  //       this.setState({ searchQuery: "" });
+//  //     }
+//  //   }
+//  //
+//  // }
+//
+//
+//   handleMarkerClick(listing) {
+//     this.props.history.push(`listings/${listing.id}`);
+//   }
+//
+//
+//
+//   render() {
+//     if (this.MarkerManager) {
+//       this.MarkerManager.updateMarkers(this.props.listings);
+//     }
+//
+//
+//
+//     return (
+//       <div className='map' ref="map">
+//       </div>
+//     );
+//   }
+// }
+//
+// export default ListingMap;
