af805dca89b921f570100bfea6861855f3e9d063
Melissa Ho <homelissa93@gmail.com>
2019-01-05 20:45:37 -0800

    delete unnecessary extra line

--- frontend/actions/booking_actions.js
+++ frontend/actions/booking_actions.js
@@ -25,7 +25,6 @@ export const receiveErrors = errors => ({
   errors
 });
 
-
 export const createBooking = bookingParams => dispatch => (
   APIUtil.createBooking(bookingParams).then(booking => (
     dispatch(receiveBooking(booking))
